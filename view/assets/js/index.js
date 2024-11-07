3f902fc359e9b35ce28753d01420a216ebe6b96b
luccas-arruda <luccasarruda@hotmail.com.br>
2024-11-06 22:05:50 -0300

    feat: adicionar estilização de mensagens entre cliente e servidor

--- view/assets/js/index.js
+++ view/assets/js/index.js
@@ -1,38 +1,41 @@
-    const ws = new WebSocket("ws://localhost:8081");
-    let nomeDoCabra       
-    ws.onopen = () => {
-        console.log("Conectado ao servidor WebSocket");
-        nomeDoCabra = window.prompt("Digite seu nome:")
-        console.log(nomeDoCabra)
-    };
+const ws = new WebSocket("ws://localhost:8081");
 
-    ws.onmessage = (event) => {
-        const chatLog = document.getElementById("chatLog");
-        const message = document.createElement("div");
-        message.textContent = "Servidor " + event.data;
-        chatLog.appendChild(message);
-    };
+ws.onopen = () => {
+    console.log("Conectado ao servidor WebSocket");
+};
 
-    ws.onclose = () => {
-        console.log("Conexão fechada com o servidor WebSocket");
-    };
+ws.onmessage = (event) => {
+    const chatLog = document.getElementById("chatLog");
+    const message = document.createElement("div");
+    message.textContent = "Servidor: " + event.data;
+    message.classList.add("server"); // Adiciona a classe 'server' para estilizar
+    chatLog.appendChild(message);
 
-    function sendMessage() {
-        const input = document.getElementById("messageInput");
-        const message = input.value;
+    // Scroll para a última mensagem
+    chatLog.scrollTop = chatLog.scrollHeight;
+};
+
+function sendMessage() {
+    const input = document.getElementById("messageInput");
+    const message = input.value;
+    if (message.trim() !== "") {
         ws.send(message);
 
         const chatLog = document.getElementById("chatLog");
         const userMessage = document.createElement("div");
-        userMessage.textContent = `${nomeDoCabra}: ${message}`
+        userMessage.textContent = `${message}`;
+        userMessage.classList.add("client"); // Adiciona a classe 'client' para estilizar
         chatLog.appendChild(userMessage);
 
-        input.value = "";
+        input.value = ""; // Limpa o campo de entrada
+        chatLog.scrollTop = chatLog.scrollHeight; // Scroll automático para o final
     }
+}
 
-    document.getElementById('messageInput').addEventListener('keydown', function (event) {
-        if (event.key === 'Enter') {
-            event.preventDefault(); 
-            sendMessage();
-        }
-    });
+// Envio com Enter
+document.getElementById('messageInput').addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        sendMessage();
+    }
+});
